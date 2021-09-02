ce7c90f37cbbf7eaa3d2bf31b02cd0fc85fb1634
rezwan hossain <rezwanvoot@gmail.com>
2021-09-02 16:08:02 +0600

    added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,40 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
-  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: colors.black,
+        white: colors.white,
+        gray: colors.coolGray,
+        red: colors.red,
+        yellow: colors.amber,
+        orange: colors.orange,
+        green: colors.emerald,
+        blue: colors.blue,
+        indigo: colors.indigo,
+        purple: colors.violet,
+        pink: colors.pink,
+        rose: colors.rose,
+        teal: colors.teal,
+        primary: "#005059",
+        customGray: {
+          450: "#BEBEBE",
+          550: "#707070",
+        },
+        primaryLight: "#0050590d",
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+};
