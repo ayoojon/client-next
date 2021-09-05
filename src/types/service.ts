036f18c5430bc1caf6147276a7ee32a55b74a31e
Ariful Alam <alamariful1727@gmail.com>
2021-09-05 21:39:29 +0600

    New Types

---
+++ src/types/service.ts
@@ -0,0 +1,130 @@
+import { IUser } from './user';
+
+export type serviceTypes =
+  | 'venue'
+  | 'event-management'
+  | 'photographer'
+  | 'caterings'
+  | 'flowers'
+  | 'music'
+  | 'invitation-card'
+  | 'lightening'
+  | 'videographer'
+  | 'honeymoon';
+
+export interface IService {
+  _id: string;
+  url: string;
+  name: string;
+  address: string;
+  email: string;
+  weekDays: any;
+  contactNo: string;
+  ownerName: string;
+  ownerEmail: string;
+  type: serviceTypes;
+  logo: File | string | undefined;
+  coverImage: string;
+  advanceBookingTime: 'any' | '1m' | '3m';
+  maxBookingPeriod: number;
+  avgRating: number;
+  placeDescription: string;
+  termsConditions: string;
+  location: {
+    country: string;
+    address: string;
+    apartmentSuite: string;
+    city: string;
+    state: string;
+    zipCode: number;
+  };
+  amenities: {
+    title: string;
+    options: string[];
+  };
+  albums: {
+    _id: string;
+    title: string;
+    description: string;
+    photos: string[];
+  }[];
+  paymentMethod: {
+    bank?: {
+      name: string;
+      cardNumber: string;
+      expireDate: string;
+      CVC: number;
+    };
+    bKash?: {
+      number: string;
+    };
+  };
+  pricing: {
+    type: 'location' | 'package' | 'product';
+    location: {
+      _id: string;
+      type: 'single' | 'entire';
+      title: string;
+      description: string;
+      capacity: number;
+      size: {
+        amount: number;
+        unit: string;
+      };
+      photos: string[];
+      sessions: {
+        _id: string;
+        startTime: string;
+        endTime: string;
+        price: {
+          amount: number;
+          currency: 'bdt';
+        };
+      }[];
+    }[];
+    package: {
+      _id: string;
+      title: string;
+      description: string;
+      image: string;
+      price: {
+        amount: number;
+        currency: 'bdt';
+      };
+    }[];
+    product: {
+      minGuest: number;
+      minPrice: {
+        amount: number;
+        currency: 'bdt';
+      };
+      categories: {
+        _id: string;
+        title: string;
+        items: {
+          _id: string;
+          title: string;
+          description: string;
+          image: string;
+          price: {
+            amount: number;
+            currency: 'bdt';
+          };
+        }[];
+      }[];
+    };
+  };
+  isEmailVerified: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IReviewService {
+  booking: {
+    account: Pick<IUser, '_id' | 'name' | 'avatar'>;
+  };
+  rating: Number;
+  text?: string;
+  createdAt: string;
+  updatedAt: string;
+}
---
+++ src/types/user.ts
@@ -0,0 +1,43 @@
+import { IEvent } from './event';
+import { serviceTypes } from './service';
+
+export interface IUserServicesList {
+  _id: string;
+  name: string;
+  logo: string;
+  type: serviceTypes;
+  isVerified: boolean;
+}
+
+export interface IUserEventsList {
+  _id: IEvent['_id'];
+  name: IEvent['name'];
+  coverImage: IEvent['coverImage'];
+  type: IEvent['type'];
+  status: IEvent['status'];
+}
+
+export interface IUser {
+  _id: string;
+  email: string;
+  name: string;
+  type: 'admin' | 'user';
+  isVerified: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  services: IUserServicesList[];
+  events: IUserEventsList[];
+  methods: string[];
+  google?: {
+    id?: string;
+  };
+  contactNo?: string;
+  avatar?: string;
+  address?: string;
+  location?: string;
+}
+
+export interface ICurrentPosition {
+  latitude: number;
+  longitude: number;
+}
