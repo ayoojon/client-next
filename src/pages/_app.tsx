f6c36b306e805f1b6cc56701cfeb148e300f0659
Ariful Alam <alamariful1727@gmail.com>
2021-09-22 22:50:17 +0600

    nextjs-progressbar added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import React from 'react';
+import { Router } from 'next/router';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MainLayout from 'src/layouts/main';
@@ -7,25 +8,21 @@ import { Provider } from 'react-redux';
 import { store } from 'src/stores';
 import { ThemeProvider } from '@mui/material';
 import { theme } from '@/config/mui';
+import NextNprogress from 'nextjs-progressbar';
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-dates/lib/css/_datepicker.css';
 import './../styles/globals.css';
 import './../styles/tailwind.css';
-import 'nprogress/nprogress.css';
-import NProgress from 'nprogress';
-import { Router } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
-  Router.events.on('routeChangeStart', () => NProgress.start());
-  Router.events.on('routeChangeComplete', () => NProgress.done());
-  Router.events.on('routeChangeError', () => NProgress.done());
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <QueryClientProvider client={queryClient}>
           <MainLayout>
+            <NextNprogress color="#005059" startPosition={0.1} stopDelayMs={200} height={3} showOnShallow={false} />
             <Component {...pageProps} />
           </MainLayout>
           {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
